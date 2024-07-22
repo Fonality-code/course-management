39bf5d5df330724220a058a8765324bc038f4f44
Fon Godwill Ivan Tana <ivan8tana@gmail.com>
2024-07-22 15:49:21 +0100

    chore: Update base template title and remove unused config file

--- app.py
+++ app.py
@@ -8,3 +8,25 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/calendar")
+def calendar():
+    return render_template("calendar.html")
+
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
+@app.route("/signup")
+@app.route("/register")
+def signup():
+    return render_template("auth/signup.html")
+
+
+@app.route("/signin")
+@app.route("/login")
+def signin():
+    return render_template("auth/signin.html")
